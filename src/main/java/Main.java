baf460aa835bacdb8a88e28d05c91cd138bd00eb
Julia Kelman <kelmanyulia@gmail.com>
2021-10-29 22:35:20 -0400

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,22 +13,3 @@ public class Main {
 
 
 
-
-//Main startGame=new Main();
-
-//        Player Player1= new Player ("Moshe", 1);
-//
-//        Player Player2= new Player("Yossi", 2);
-//
-//
-//        Dice rollDice=new Dice(6);
-//        rollDice.roll();
-//        System.out.println(Player1+ " "+ Dice.playerRolls);
-//        System.out.println(Player2+ " "+ Dice.playerRolls);
-//
-//        System.out.println(Player1+ " "+ Dice.playerRolls);
-//        System.out.println(Player2+ " "+ Dice.playerRolls);
-//
-//        //Game game1=new Game();
-//        game1.compareScore(Player1,Player2);
-
